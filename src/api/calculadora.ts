c79c455ca9a56c9534f3fc8ae24986434f372479
DanielLubo <danielfelipelubo@gmail.com>
2026-05-15 18:12:38 -0500

    organizacion

--- src/api/calculadora.ts
+++ src/api/calculadora.ts
@@ -4,9 +4,6 @@ import axiosInstance from './axiosInstance';
 export const simplificarFraccion = (numerador: number, denominador: number, esNegativo: boolean) =>
     axiosInstance.post<ResultadoOperacion>('/convertir/simplificar', { numerador, denominador, esNegativo });
 
-export const convertirDecimal = (numero: string, esNegativo: boolean) =>
-    axiosInstance.post<ResultadoOperacion>('/convertir/decimal', { numero, esNegativo });
-
 export const convertirMixto = (
     numeroEntero: number,
     numerador: number,
@@ -19,6 +16,9 @@ export const convertirMixto = (
         denominador,
         esNegativo,
     });
+
+export const convertirDecimal = (numero: string, esNegativo: boolean) =>
+    axiosInstance.post<ResultadoOperacion>('/convertir/decimal', { numero, esNegativo });
 
 export const convertirNumeroPeriodicoPuro = (numeroEntero: string, numeroPeriodico: string, esNegativo: boolean) =>
     axiosInstance.post<ResultadoOperacion>('/convertir/periodico-puro', { numeroEntero, numeroPeriodico,esNegativo });
